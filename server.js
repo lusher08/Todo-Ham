9ec934b3b475e5636cc961347cd8ef4af40ccf18
lusher08 <lusher08@yonsei.ac.kr>
2026-03-01 06:42:19 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express   = require('express');
+const basicAuth = require('express-basic-auth');
+const path      = require('path');
+const app       = express();
+
+app.use(basicAuth({
+    users: { 'minutapita': '0925' },
+    challenge: true,
+    realm: '🐹 Todo Ham'
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(process.env.PORT || 3000);
